9ea9b7e87c3de48e3756637863014a80ea1d8030
William Boudreault <boudreaultwilliam@gmail.com>
2024-08-21 22:33:25 -0400

    Markov chains (WIP) works but node values need to be worked out

--- src/name_generator.rs
+++ src/name_generator.rs
@@ -1,33 +1,78 @@
-use std::collections::HashMap;
+use rand::{seq::IteratorRandom, Rng};
 
-use rand::{rngs::ThreadRng, seq::IteratorRandom, Rng};
+struct MarkovLink {
+    pub state: char,
+    pub p: u8,
+}
 
-/// Generates a name using Markov Chains
+struct MarkovChain {
+    nodes: [Vec<MarkovLink>; 26],
+}
+
+impl MarkovChain {
+    pub fn new() -> Self {
+        const ARRAY_REPEAT_VALUE: Vec<MarkovLink> = Vec::new();
+        let nodes = [ARRAY_REPEAT_VALUE; 26];
+        return MarkovChain { nodes };
+    }
+
+    pub fn push_link(&mut self, start_state: char, next_state: char, p: u8) {
+        let link = MarkovLink {
+            state: next_state,
+            p,
+        };
+        self.nodes[(start_state as u8 - b'a') as usize].push(link);
+    }
+
+    pub fn next_state(&self, state: char, x: u8) -> char {
+        let links = &self.nodes[(state as u8 - b'a') as usize];
+
+        let mut p = 0;
+        for link in links {
+            p += link.p;
+            if x <= p {
+                return link.state;
+            }
+        }
+
+        return '_';
+    }
+}
+
+/// Generates a name using a Markov Chain
 pub fn generate_character_name() -> String {
     let mut rng = rand::thread_rng();
     let mut name = String::new();
 
-    name.push_str(generate_name_syllables(&mut rng));
+    let mut state = ('a'..'z').choose(&mut rng).unwrap();
 
-    let mut c = name.chars();
-    let first_char = c.next().unwrap();
+    let chain = parse_markov_file();
 
-    name = first_char.to_uppercase().chain(c).collect();
+    name.push(state.to_ascii_uppercase());
 
-    for _ in 0..(rng.gen_range(1..5)) {
-        name.push_str(generate_name_syllables(&mut rng));
+    while state != '_' {
+        state = chain.next_state(state, rng.gen());
+        name.push(state);
     }
 
     return name;
 }
 
-/// Randomly picks a syllable from a list
-fn generate_name_syllables(rng: &mut ThreadRng) -> &str {
-    let syllables = include_str!("../name_syllables.txt");
-    return syllables
-        .lines()
-        .choose(rng)
-        .expect("Could not choose syllable randomly.");
+fn parse_markov_file() -> MarkovChain {
+    let file = include_str!("../markov_nodes.txt");
+
+    let mut chain = MarkovChain::new();
+
+    file.lines().for_each(|line| {
+        let mut split = line.split(' ');
+        let start_state = split.next().unwrap().chars().next().unwrap();
+        let next_state = split.next().unwrap().chars().next().unwrap();
+        let p = u8::from_str_radix(split.next().unwrap(), 10).unwrap();
+
+        chain.push_link(start_state, next_state, p);
+    });
+
+    return chain;
 }
 
 #[test]
